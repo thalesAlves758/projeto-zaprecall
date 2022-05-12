5217b1bf5c050a9e65ca8c2af3f287ab77030e86
Thales Alves <thalesalves758@gmail.com>
2022-05-12 02:10:21 -0300

    Adicionar a parte de ion-icons na barra inferior

--- src/components/BottomBar.js
+++ src/components/BottomBar.js
@@ -4,6 +4,11 @@ export default function BottomBar () {
       <div className="completed">
         <h3>0/8 CONCLUÍDOS</h3>
       </div>
+      <div className="answer-icons">
+        {/* <ion-icon name="close-circle"></ion-icon>
+        <ion-icon name="help-circle"></ion-icon>
+        <ion-icon name="checkmark-circle"></ion-icon> */}
+      </div>
     </div>
   );
 }
