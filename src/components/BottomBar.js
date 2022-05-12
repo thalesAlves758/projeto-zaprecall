80cf6a299b956206284b68324697430002b87e93
Thales Alves <thalesalves758@gmail.com>
2022-05-12 01:48:25 -0300

    Criar componente BottomBar

---
+++ src/components/BottomBar.js
@@ -0,0 +1,9 @@
+export default function BottomBar () {
+  return (
+    <div className="bottom-bar">
+      <div className="completed">
+        <h3>0/8 CONCLUÍDOS</h3>
+      </div>
+    </div>
+  );
+}
