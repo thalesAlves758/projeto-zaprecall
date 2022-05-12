70cca1a792cf2b8f1b68d14a822b60edda9c1cca
Thales Alves <thalesalves758@gmail.com>
2022-05-12 01:50:30 -0300

    Importar MainContent

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import StartDisplay from "./components/StartDisplay";
+import InitialDisplay from "./components/InitialDisplay";
+import MainContent from './components/MainContent';
 
 export default function App () {
   return (
-    <div className="container">
-      <StartDisplay />
-    </div>
+    <>
+      {/* <InitialDisplay /> */}
+      <MainContent />
+    </>
   );
 }
