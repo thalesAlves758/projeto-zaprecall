54fab612885c363ae8ecaa4347de1801a03153c1
Thales Alves <thalesalves758@gmail.com>
2022-05-13 12:10:51 -0300

    Adicionar array de decks

--- src/components/MainDisplay.js
+++ src/components/MainDisplay.js
@@ -5,10 +5,40 @@ import Footer from './Footer';
 import EndMessage from './EndMessage';
 
 export default function MainDisplay() {
+  const decks = [
+    [
+      {
+        question: "O que é JSX?",
+        answer: "Uma extensão de linguagem do JavaScript",
+      }, {
+        question: "O React é __",
+        answer: "uma biblioteca JavaScript para construção de interfaces",
+      }, {
+        question: "Componentes devem iniciar com __",
+        answer: "letra maiúscula",
+      }, {
+        question: "Podemos colocar __ dentro do JSX",
+        answer: "expressões",
+      }, {
+        question: "O ReactDOM nos ajuda __",
+        answer: "interagindo com a DOM para colocar componentes React na mesma",
+      }, {
+        question: "Usamos o npm para __",
+        answer: "gerenciar os pacotes necessários e suas dependências",
+      }, {
+        question: "Usamos props para __",
+        answer: "passar diferentes informações para componentes",
+      }, {
+        question: "Usamos estado (state) para __",
+        answer: "dizer para o React quais informações quando atualizadas devem renderizar a tela novamente",
+      },
+    ]
+  ];
+
   return (
     <div className="content">
       <Logo />
-      <Flashcards />
+      <Flashcards deck={decks[0]} />
       <Footer>
         {/* <EndMessage rememberedAll={true} /> */}
         <BottomInfo />
